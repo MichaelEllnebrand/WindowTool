0fd2da3dcf2b068aae1f5e0c189b73085b3c7a20
Michael Ellnebrand <denatus@denatus.com>
2022-10-02 15:26:55 +0200

    Hard coded move and resize hotkeys

--- HotkeyHandler.cs
+++ HotkeyHandler.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
+using System.Linq.Expressions;
 using System.Text;
 using GlobalLowLevelHooks;
 
@@ -13,9 +15,15 @@ namespace WindowTool
 
         private bool isMovingWindow;
         private bool isResizingWindow;
-            
 
-        internal void Start()
+        private bool isLeftControlDown;
+        private bool isLeftWinDown;
+        private bool isLeftAltDown;
+
+
+
+
+    internal void Start()
         {
             mouseHook = new MouseHook();
             mouseHook.MouseMove += MouseHook_MouseMove;
@@ -45,10 +53,61 @@ namespace WindowTool
 
         private void KeyboardHook_KeyDown(KeyboardHook.VKeys key)
         {
+            if (key == KeyboardHook.VKeys.LCONTROL) isLeftControlDown = true;
+            if (key == KeyboardHook.VKeys.LWIN) isLeftWinDown = true;
+            if (key == KeyboardHook.VKeys.LMENU) isLeftAltDown = true;
+            CheckHotKeyDown();
         }
 
         private void KeyboardHook_KeyUp(KeyboardHook.VKeys key)
         {
+            if (key == KeyboardHook.VKeys.LCONTROL) isLeftControlDown = false;
+            if (key == KeyboardHook.VKeys.LWIN) isLeftWinDown = false;
+            if (key == KeyboardHook.VKeys.LMENU) isLeftAltDown = false;
+            CheckHotKeyDown();
+        }
+
+        private void CheckHotKeyDown()
+        {
+            if (isLeftControlDown == true && isLeftWinDown == true)
+            {
+                // save window
+
+                if (isLeftAltDown == false && isMovingWindow == false) StartMovingWindow();
+                if (isLeftAltDown == true && isResizingWindow == false) StartResizingWindow();
+            }
+            else
+            {
+                if (isMovingWindow) StopMovingWindow();
+                if (isResizingWindow) StopResizingWindow();
+            }
+
+            Debug.WriteLine($"isMovingWindow {isMovingWindow} isResizingWindow {isResizingWindow}");
+        }
+
+
+        private void StartMovingWindow()
+        {
+            isMovingWindow = true;
+            isResizingWindow = false;
+        }
+
+        private void StopMovingWindow()
+        {
+            isMovingWindow = false;
+
+        }
+
+
+        private void StartResizingWindow()
+        {
+            isMovingWindow = false;
+            isResizingWindow = true;
+        }
+
+        private void StopResizingWindow()
+        {
+            isResizingWindow = false;
         }
     }
 }
